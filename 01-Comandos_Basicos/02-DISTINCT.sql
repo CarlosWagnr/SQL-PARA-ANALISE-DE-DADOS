24c5dd0e7e11953e8ef9b6398551b192a96e72c5
Carlos Wagner <carlos.alves.conceicao@hotmail.com>
2025-02-25 21:40:07 -0300

    Comandos Básicos SQL

---
+++ 01-Comandos_Basicos/02-DISTINCT.sql
@@ -0,0 +1,23 @@
+-- DISTINCT - Comando usado para remover linhas duplicadas e mostrar apenas linhas distintas.
+-- SINTAXE 
+select distinct coluna_1, coluna_2, coluna_3
+from schema_1.tabela_1;
+
+
+-- EXEMPLOS
+-- (Exemplo 1) Seleção de uma coluna com DISTINCT
+-- Liste as marcas de carro distintas que constam na tabela products
+select distinct brand
+from sales.products;
+
+
+-- (Exemplo 2) Seleção de mais de uma coluna com DISTINCT
+-- Liste as marcas e anos de modelo distintos que constam na tabela products
+select distinct brand, model_year
+from sales.products;
+
+
+-- RESUMO
+-- (1) Comando usado para remover linhas duplicadas e mostrar apenas linhas distintas.
+-- (2) Caso mais de uma coluna seja selecionada, o comando SELECT DISTINCT irá 
+-- retornar todas as combinações distintas.
