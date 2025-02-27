7b784a20ddf608a9465c8ebdd3107900ad3c0bb7
Carlos Wagner <carlos.alves.conceicao@hotmail.com>
2025-02-26 22:15:15 -0300

    Operadores SQL

--- 01-Comandos_Basicos/06-DESAFIO.sql
+++ 01-Comandos_Basicos/06-DESAFIO.sql
@@ -2,7 +2,6 @@
 
 -- (Exercício 1) Selecione os nomes de cidades distintas que existem no estado de
 -- Minas Gerais em ordem alfabética (dados da tabela sales.customers)
-
 select distinct city
 from sales.customers
 where state = 'MG'
@@ -11,16 +10,15 @@ order by city;
 
 -- (Exercício 2) Selecione o visit_id das 10 compras mais recentes efetuadas
 -- (dados da tabela sales.funnel)
-
 select visit_id 
 from sales.funnel 
 where paid_date is not null
 order by paid_date desc
 limit 10;
 
+
 -- (Exercício 3) Selecione todos os dados dos 10 clientes com maior score nascidos
 -- após 01/01/2000 (dados da tabela sales.customers)
-
 select *
 from sales.customers
 where birth_date >= '2000-01-01'
