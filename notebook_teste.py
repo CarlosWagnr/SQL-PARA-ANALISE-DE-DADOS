e0ad93b37b013e2cd765350c2a8b6166ec824939
carlos.alves.conceicao@hotmail.com <carlos.alves.conceicao@hotmail.com>
2026-06-03 01:02:20 +0000

    teste

---
+++ notebook_teste.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
+# DBTITLE 1,Cell 1
+print("Hello World!")
+print(3+9)
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
